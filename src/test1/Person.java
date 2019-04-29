8215449bda4111a667e4d23a7a77c69c0ccce2f4
TAAGBHI1 <pc907>
2019-04-29 15:46:45 +0200

    Here

---
+++ src/test1/Person.java
@@ -0,0 +1,18 @@
+package test1;
+
+public class Person {
+
+
+    public static void main(String args[]) {
+
+        getPerson();
+
+
+    }
+
+    private static void getPerson() {
+        String firstName;
+        String lastName;
+        int age;
+    }
+}
